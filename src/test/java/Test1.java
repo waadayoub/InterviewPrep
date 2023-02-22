98d3f726fcce336d05e263f0fd6ac4fa63183c58
waadayoub <113552101+waadayoub@users.noreply.github.com>
2023-02-22 17:34:10 -0600

    interview prep

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hi!");
         System.out.println("there");
+        System.out.println("test1");
+        System.out.println("test2");
     }
 
    }
